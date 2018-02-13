08e439897d1b4bb30fe3cf325fbf30fc41a6a452
Jing Hao <haojing9058@gmail.com>
2018-02-12 21:34:02 -0800

    testing bubble chart

--- server.py
+++ server.py
@@ -20,16 +20,16 @@ def display_userinfo():
     username1 = request.form.get('username1')
     username2 = request.form.get('username2')
 
-    # helper function to get the top 9 photo
     def helper(username):
+        """helper function to get the top 9 photo"""
             #get user id from Flickr api
-            user = request_api.get_userid_by_username(username)
-            #get photo detail data from api and save to db
-            request_api.seed_photos_by_userid(user.user_id)
-            #select 9 photos from db
-            url_list = db.session.query(Photo.url).filter(Photo.username == username).limit(9).all()
-            # flat result to a list
-            return [e for l in url_list for e in l]
+        user = request_api.get_userid_by_username(username)
+        #get photo detail data from api and save to db
+        request_api.seed_photos_by_userid(user.user_id)
+        #select 9 photos from db
+        url_list = db.session.query(Photo.url).filter(Photo.username == username).limit(9).all()
+        # flat result to a list
+        return [e for l in url_list for e in l]
 
     url_list1 = helper(username1)
     url_list2 = helper(username2)
@@ -39,7 +39,34 @@ def display_userinfo():
                             username2 = username2,
                             urls1 = url_list1,
                             urls2 = url_list2)
-    # user_id1 = request_api.get_user_by_username(username2)
+   
+@app.route('/tags-bubble')
+def display_tags_bubble():
+    return render_template('tags-bubble.html')
+# @app.route('/text-visual')
+# def visualize_texts():
+#     """visualize title, description and tags."""
+#     username1 = request.form.get('username1')
+#     username2 = request.form.get('username2')
+
+
+#     # user1_id = request.args.get('user1_id')
+#     # user2_id = request.args.get('user2_id')
+
+#     # photo_ids_1 = request_api.get_photos_by_userid(user1_id, sort='views', per_page=30)
+#     # for photo_id in photo_ids_1:
+#     #     photo = request_api.get_photo_by_photoid(photo_id)
+#     #     db_utils.add_photo(photo)
+
+#     # photo_ids_2 = request_api.get_photos_by_userid(user2_id, sort='views', per_page=30)
+#     # for photo_id in photo_ids_2:
+#     #     photo = request_api.get_photo_by_photoid(photo_id)
+#     #     db_utils.add_photo(photo)
+
+
+#     return render_template('text-visual.html')
+
+ # user_id1 = request_api.get_user_by_username(username2)
 
     # #add user into db
     # db_utils.create_user(user1)
@@ -66,27 +93,6 @@ def display_userinfo():
 
 
     
-
-
-@app.route('/text-visual')
-def visualize_texts():
-
-    # user1_id = request.args.get('user1_id')
-    # user2_id = request.args.get('user2_id')
-
-    # photo_ids_1 = request_api.get_photos_by_userid(user1_id, sort='views', per_page=30)
-    # for photo_id in photo_ids_1:
-    #     photo = request_api.get_photo_by_photoid(photo_id)
-    #     db_utils.add_photo(photo)
-
-    # photo_ids_2 = request_api.get_photos_by_userid(user2_id, sort='views', per_page=30)
-    # for photo_id in photo_ids_2:
-    #     photo = request_api.get_photo_by_photoid(photo_id)
-    #     db_utils.add_photo(photo)
-
-
-    return render_template('text-visual.html')
-
 
 
 if __name__ == "__main__":
--- word_count.py
+++ word_count.py
@@ -7,62 +7,74 @@ from model import connect_to_db, db
 import request_api
 
 def get_word_count(username):
+    """Return tag words and count of given user."""
     tags = db.session.query(Photo.tags).filter(Photo.username == username).all()
     tag_ls = [e for l in tags for e in l]
     word_ls = [word for sentence in tag_ls for word in sentence.split()]
     counts = Counter(word_ls)
     word_count_df = pd.DataFrame.from_dict(counts, orient='index').reset_index()
     word_count_df['user'] = pd.Series(username, index=word_count_df.index)
+    #sort by count and select top 30
+
     word_count_df.columns=['word','count','user']
     return word_count_df
 
+tag_words1 = get_word_count(username1)
+tag_words2 = get_word_count(username2)
+
+df = get_word_count('chrisimmler')
+df.to_csv(path_or_buth='test.csv' columns=df.columns)
+"""encoding : string, optional A string representing the encoding to use in the 
+output file, defaults to ‘ascii’ on Python 2 and ‘utf-8’ on Python 3."""
 
 
 
-engine = create_engine('postgres:///flickrgram')
-
-# users = pd.read_sql_table('users', engine, columns=['user_id','username'])
-
-# The following done through server.py
-# #given user_id, sort, and per_page, get photos obj
-# photo_ids= request_api.get_photos_by_userid(user_id, sort, per_page)
-
-# #get each photo's info and add to db
-# for id in photo_ids:
-#     photo = request_api.get_photo_by_photoid(photo)
-#     db_utils.add_photo(photo)
-
-def preprocess (str):
-    """ Lower case and strip punctuations of a string.
-    """
-    for p in list(punctuation):
-        return str.lower().replace(p, '')
-
-def 
-
-def get_df(username1, username2):
-    """
-    """
-    # Get title, description, tags of photos of username1 and username2
-    df = pd.read_sql_table('photos', engine, columns=['photo_id',
-            'title', 'description', 'tags', 'username'])
-    df = df[df['username'].isin([username1, username2])]
-    # Preprocess title, description, and tags
-    df['title'] = df.title.apply(preprocess)
-    df['description'] = df.description.apply(preprocess)
-    df['tags'] = df.tags.apply(preprocess)
-
-
-    words = pd.DataFrame(columns=['word', 'count_user1', 'count_use2', 'count_total'])
-
-    # if word in
 
 
 
-#convert 'tags' column as list of strings
-photos['tags'] = photos.tags.apply(preprocess)
+
+# engine = create_engine('postgres:///flickrgram')
+
+# # users = pd.read_sql_table('users', engine, columns=['user_id','username'])
+
+# # The following done through server.py
+# # #given user_id, sort, and per_page, get photos obj
+# # photo_ids= request_api.get_photos_by_userid(user_id, sort, per_page)
+
+# # #get each photo's info and add to db
+# # for id in photo_ids:
+# #     photo = request_api.get_photo_by_photoid(photo)
+# #     db_utils.add_photo(photo)
+
+# def preprocess (str):
+#     """ Lower case and strip punctuations of a string.
+#     """
+#     for p in list(punctuation):
+#         return str.lower().replace(p, '')
 
 
-#added 100 or more photos/tags info from server
-photos.merge()
+# def get_df(username1, username2):
+#     """
+#     """
+#     # Get title, description, tags of photos of username1 and username2
+#     df = pd.read_sql_table('photos', engine, columns=['photo_id',
+#             'title', 'description', 'tags', 'username'])
+#     df = df[df['username'].isin([username1, username2])]
+#     # Preprocess title, description, and tags
+#     df['title'] = df.title.apply(preprocess)
+#     df['description'] = df.description.apply(preprocess)
+#     df['tags'] = df.tags.apply(preprocess)
+
+
+#     words = pd.DataFrame(columns=['word', 'count_user1', 'count_use2', 'count_total'])
+
+
+
+
+# #convert 'tags' column as list of strings
+# photos['tags'] = photos.tags.apply(preprocess)
+
+
+# #added 100 or more photos/tags info from server
+# photos.merge()
 
